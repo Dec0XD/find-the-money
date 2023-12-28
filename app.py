1a78c5710256fba7847d8c02e2a21593a3de2884
DecoCorp <150845448+DecoCorp@users.noreply.github.com>
2023-12-28 10:48:48 -0300

    V 0.1.10
    - Modificação do nome da informação inicial
    - Criação da mensagem de explicação do jogo
    - Testes do retorno de valor do entry

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.resizable(0, 0)
 
 set_appearance_mode("dark")
 
-def show_information():
+def MostrarInformaçao():
     CTkMessagebox(title="Informações sobre a aplicação", 
                   message="Informações.")
 
@@ -23,10 +23,24 @@ CTkLabel(master=sidebar_frame, text="", image=logo_img).pack(pady=(38, 0), ancho
 CTkLabel(master=sidebar_frame, text="Para mais informações").pack(pady=(38, 0), anchor="center")
 CTkLabel(master=sidebar_frame, text="clique aqui").pack(pady=(2, 0), anchor="center")
 
-information = CTkButton(master=sidebar_frame, text="informações", command=show_information).pack(pady=(10,0), anchor="center")
+information = CTkButton(master=sidebar_frame, text="informações", command=MostrarInformaçao).pack(pady=(10,0), anchor="center")
+
+def InfosJogo():
+    CTkMessagebox(title="Informações sobre o jogo", 
+                message="""
+Bem vindo! Vamos dar início ao jogo.\n
+Para jogar é bem simples:\n
+1. Escolha uma caixinha branca, ou da esquerda ou da direita, e tente encontrar a nota de 100 reais.
+2. No início de cada rodada posicione o mouse no círculo vermelho.
+3. Cuidado! Se demorar muito para escolher, voltará para o início do jogo."""
+                )
+
 
 def IniciarJogo():
-    pass
+    NotaEsquerda = QuantidadeNotaEsquerda.get()
+    NRodadas = NumeroRodadas.get()
+    print(NotaEsquerda)
+    print(NRodadas)
     
 CTkLabel(master=app, text="Configuração", text_color="#fff",
         justify="center", font=("Arial Bold", 24)).pack(anchor="center", pady=(0, 5),padx=(25, 0))
@@ -43,7 +57,7 @@ NumeroRodadas.pack(padx=20, pady=20)
 
 CTkLabel(master=app, text="Aperte o botão para iniciar o jogo!", text_color="#fff",
         justify="center", font=("Arial Bold", 16)).pack(anchor="center", pady=(0, 5),padx=(25, 0))  
-inciarJogo = CTkButton(master=app, text="inicar Jogo!", text_color="#fff", command=IniciarJogo).pack(anchor="center")
+inciarJogo = CTkButton(master=app, text="inicar Jogo!", text_color="#fff", command=InfosJogo).pack(anchor="center")
 
 
 
