bf33e8b14e8421fe779af5c94ab71c0e599da68f
DecoCorp <150845448+DecoCorp@users.noreply.github.com>
2023-12-28 09:47:24 -0300

    V 0.1.1
    - Sidebar com cor azul

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ app.resizable(0, 0)
 set_appearance_mode("dark")
 
 
+sidebar_frame = CTkFrame(master=app, fg_color="#070F1F", width=200, height=800, corner_radius=0)
+sidebar_frame.pack_propagate(0)
+sidebar_frame.pack(fill="y", anchor="w", side="left")
+
 
 app.mainloop()
