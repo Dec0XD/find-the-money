973dd1c4b27481b1f048bc853e1c1344b76b60a5
DecoCorp <150845448+DecoCorp@users.noreply.github.com>
2023-12-28 09:46:22 -0300

    V 0.1.0
    - Tela inicial
    - Documentação em txt

---
+++ app.py
@@ -0,0 +1,13 @@
+from customtkinter import *
+from CTkMessagebox import CTkMessagebox
+from PIL import Image
+
+app = CTk()
+app.geometry("900x600")
+app.resizable(0, 0)
+
+set_appearance_mode("dark")
+
+
+
+app.mainloop()
