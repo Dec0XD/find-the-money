3d5589c8e384ad6221d94b34cf7438521b1e348b
DecoCorp <150845448+DecoCorp@users.noreply.github.com>
2023-12-28 11:19:08 -0300

    V 0.1.11
    - Colocando as informações do jogo no botão do sidebar
    - Criando os botões do jogo

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ set_appearance_mode("dark")
 def MostrarInformaçao():
     CTkMessagebox(title="Informações sobre a aplicação", 
                   message="Informações.")
+def InfosJogo():
+    CTkMessagebox(title="Informações sobre o jogo", 
+                message="""
+Bem vindo! Vamos dar início ao jogo.\n
+Para jogar é bem simples:\n
+1. Escolha uma caixinha branca, ou da esquerda ou da direita, e tente encontrar a nota de 100 reais.
+2. No início de cada rodada posicione o mouse no círculo vermelho.
+3. Cuidado! Se demorar muito para escolher, voltará para o início do jogo."""
+                )
 
 sidebar_frame = CTkFrame(master=app, fg_color="#070F1F", width=200, height=800, corner_radius=0)
 sidebar_frame.pack_propagate(0)
@@ -23,24 +32,18 @@ CTkLabel(master=sidebar_frame, text="", image=logo_img).pack(pady=(38, 0), ancho
 CTkLabel(master=sidebar_frame, text="Para mais informações").pack(pady=(38, 0), anchor="center")
 CTkLabel(master=sidebar_frame, text="clique aqui").pack(pady=(2, 0), anchor="center")
 
-information = CTkButton(master=sidebar_frame, text="informações", command=MostrarInformaçao).pack(pady=(10,0), anchor="center")
+informationApp = CTkButton(master=sidebar_frame, text="informações", command=MostrarInformaçao).pack(pady=(10,0), anchor="center")
+informationJogo = CTkButton(master=sidebar_frame, text="Jogo", command=InfosJogo).pack(pady=(10,0), anchor="center")
 
-def InfosJogo():
-    CTkMessagebox(title="Informações sobre o jogo", 
-                message="""
-Bem vindo! Vamos dar início ao jogo.\n
-Para jogar é bem simples:\n
-1. Escolha uma caixinha branca, ou da esquerda ou da direita, e tente encontrar a nota de 100 reais.
-2. No início de cada rodada posicione o mouse no círculo vermelho.
-3. Cuidado! Se demorar muito para escolher, voltará para o início do jogo."""
-                )
+
 
 
 def IniciarJogo():
-    NotaEsquerda = QuantidadeNotaEsquerda.get()
-    NRodadas = NumeroRodadas.get()
-    print(NotaEsquerda)
-    print(NRodadas)
+    pass
+    #NotaEsquerda = QuantidadeNotaEsquerda.get()
+    #NRodadas = NumeroRodadas.get()
+    #print(NotaEsquerda)
+    #print(NRodadas)
     
 CTkLabel(master=app, text="Configuração", text_color="#fff",
         justify="center", font=("Arial Bold", 24)).pack(anchor="center", pady=(0, 5),padx=(25, 0))
@@ -57,8 +60,15 @@ NumeroRodadas.pack(padx=20, pady=20)
 
 CTkLabel(master=app, text="Aperte o botão para iniciar o jogo!", text_color="#fff",
         justify="center", font=("Arial Bold", 16)).pack(anchor="center", pady=(0, 5),padx=(25, 0))  
-inciarJogo = CTkButton(master=app, text="inicar Jogo!", text_color="#fff", command=InfosJogo).pack(anchor="center")
+inciarJogo = CTkButton(master=app, text="inicar Jogo!", text_color="#fff", command=IniciarJogo).pack(anchor="center")
 
+#jogo
+# Botão da esquerda
+BTNEsquerda = CTkButton(master=app, text="Botão da esquerda")
+BTNEsquerda.pack(side="left", padx=100, pady=10)
 
+# Botão da direita
+BTNDireita = CTkButton(master=app, text="Botão da direita")
+BTNDireita.pack(side="right", padx=100, pady=10)
 
 app.mainloop()
