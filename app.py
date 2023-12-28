233d65aa11254af6648aeb7ec87c7afabbfc324f
DecoCorp <150845448+DecoCorp@users.noreply.github.com>
2023-12-28 09:52:34 -0300

    V 0.1.2
    Sidebar Atualizado
    - Foto do GPDOC
    - Texto de informações

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ app.resizable(0, 0)
 
 set_appearance_mode("dark")
 
-
 sidebar_frame = CTkFrame(master=app, fg_color="#070F1F", width=200, height=800, corner_radius=0)
 sidebar_frame.pack_propagate(0)
 sidebar_frame.pack(fill="y", anchor="w", side="left")
+
+logo_img_data = Image.open("assets/GPDOC.png")
+logo_img = CTkImage(dark_image=logo_img_data, light_image=logo_img_data, size=(100, 110))
+
+CTkLabel(master=sidebar_frame, text="", image=logo_img).pack(pady=(38, 0), anchor="center")
+CTkLabel(master=sidebar_frame, text="Para mais informações").pack(pady=(38, 0), anchor="center")
+CTkLabel(master=sidebar_frame, text="clique aqui").pack(pady=(2, 0), anchor="center")
+
 
 
 app.mainloop()
