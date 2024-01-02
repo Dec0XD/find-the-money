3eaa930c4d5fd64784cca3913d6b2ffffa45f2fd
DecoCorp <150845448+DecoCorp@users.noreply.github.com>
2024-01-02 10:27:05 -0300

    V 0.2.0
    Transformando app em exe

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from tkinter import Toplevel, Label, Canvas
 import random
 import time
 
-
 app = CTk()
 app.geometry("900x600")
 app.resizable(0, 0)
@@ -28,7 +27,7 @@ sidebar_frame = CTkFrame(master=app, fg_color="#070F1F", width=200, height=800, 
 sidebar_frame.pack_propagate(0)
 sidebar_frame.pack(fill="y", anchor="w", side="left")
 
-logo_img_data = Image.open("assets/GPDOC.png")
+logo_img_data = Image.open("assets/GPDOC.jpg")
 logo_img = CTkImage(dark_image=logo_img_data, light_image=logo_img_data, size=(100, 110))
 
 CTkLabel(master=sidebar_frame, text="", image=logo_img).pack(pady=(38, 0), anchor="center")
